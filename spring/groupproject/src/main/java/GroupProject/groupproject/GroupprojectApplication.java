0b0a063edbebda0b0de72ae43514d0f9c564d1a6
Tam Doan <mdtam147@gmail.com>
2022-05-28 19:23:03 -0500

    springboot files

---
+++ spring/groupproject/src/main/java/GroupProject/groupproject/GroupprojectApplication.java
@@ -0,0 +1,13 @@
+package GroupProject.groupproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupprojectApplication.class, args);
+	}
+
+}
---
+++ spring/groupproject/src/test/java/GroupProject/groupproject/GroupprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package GroupProject.groupproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
