edb7839efc03542437ac832134ee93c832016189
Hoang Le <hoangp.le21@gmail.com>
2022-05-31 13:02:37 -0500

    Added blog list page

--- groupproject/src/app/app.module.ts
+++ groupproject/src/app/app.module.ts
@@ -1,17 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { BlogListComponent } from './blog-list/blog-list.component';
+import { BlogPreviewComponent } from './blog-preview/blog-preview.component';
 
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    BlogListComponent,
+    BlogPreviewComponent,
+  ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([{ path: '', component: NavbarComponent }]),
+    RouterModule.forRoot([{ path: '', component: BlogListComponent }]),
     FontAwesomeModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ groupproject/src/app/blog/blog.ts
@@ -0,0 +1,8 @@
+export interface Blog {
+  id?: number;
+  imageUrl?: string;
+  title?: string;
+  author?: string;
+  date?: Date;
+  content?: string;
+}
