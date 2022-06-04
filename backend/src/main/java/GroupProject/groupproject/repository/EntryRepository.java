6472be4d3f01d5ab011ddcfe6f929af7a3cfc833
DemetriusElder <104701476+DemetriusElder@users.noreply.github.com>
2022-06-03 21:59:09 -0500

    added get by id

--- backend/src/main/java/GroupProject/groupproject/repository/EntryRepository.java
+++ backend/src/main/java/GroupProject/groupproject/repository/EntryRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface EntryRepository extends JpaRepository<Entry, Long> {
 
     List<Entry> findAll();
+    Entry getById(Long id);
 
 }
