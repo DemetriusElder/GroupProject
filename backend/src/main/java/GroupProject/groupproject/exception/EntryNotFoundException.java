6ce350f5fe1c0a84d9f1fbbe8bc31f22252f14cc
DemetriusElder <104701476+DemetriusElder@users.noreply.github.com>
2022-06-19 19:57:21 -0500

    Delete backend directory

--- backend/src/main/java/GroupProject/groupproject/exception/EntryNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package GroupProject.groupproject.exception;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Entry Not Found")
-public class EntryNotFoundException extends Exception{
-
-}
