e9153b064a1b235255050e062b536fd7b5dd1695
DemetriusElder <104701476+DemetriusElder@users.noreply.github.com>
2022-06-20 18:08:06 -0500

    Add files via upload

---
+++ backend/src/main/java/GroupProject/groupproject/repository/AuthUsersRepository.java
@@ -0,0 +1,22 @@
+package GroupProject.groupproject.repository;
+
+import GroupProject.groupproject.entity.AuthUsers;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AuthUsersRepository extends JpaRepository<AuthUsers, Long>{
+	
+	 	List<AuthUsers> findAll();
+
+	    AuthUsers getById(Long id);
+
+	    boolean existsById(Long id);
+	    
+	    @Query("SELECT e FROM AuthUsers e WHERE CONCAT(e.username, e.password, e.role) LIKE %?1%")
+	    List<AuthUsers> getFilteredAuthUsers(String searchKey);
+
+}
