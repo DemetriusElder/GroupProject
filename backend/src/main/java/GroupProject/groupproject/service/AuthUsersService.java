17ca851545481cdb74731dbec85cfe272be73979
DemetriusElder <104701476+DemetriusElder@users.noreply.github.com>
2022-06-20 18:08:26 -0500

    Add files via upload

---
+++ backend/src/main/java/GroupProject/groupproject/service/AuthUsersService.java
@@ -0,0 +1,45 @@
+package GroupProject.groupproject.service;
+
+import GroupProject.groupproject.dto.AuthUsersDto;
+import GroupProject.groupproject.entity.AuthUsers;
+import GroupProject.groupproject.exception.EntryNotFoundException;
+import GroupProject.groupproject.repository.AuthUsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class AuthUsersService {
+
+    private final AuthUsersRepository authusersRepository;
+
+    @Autowired
+    public AuthUsersService(AuthUsersRepository authusersRepository) {
+        this.authusersRepository = authusersRepository;
+    }
+
+    public List<AuthUsers> getAll() {
+        return authusersRepository.findAll();
+    }
+
+    public void addAuthUsers(AuthUsersDto authusersDto) {
+        AuthUsers authusers = new AuthUsers();
+        authusers.setUsername(authusersDto.getusername());
+        authusers.setPassword(authusersDto.getpassword());
+        authusers.setRole(authusersDto.getrole());
+        authusersRepository.save(authusers);
+    }
+    public AuthUsers getById(Long id) throws EntryNotFoundException {
+        System.out.println(id);
+        if (!authusersRepository.existsById(id)) {
+            throw new EntryNotFoundException();
+        }
+        return authusersRepository.getById(id);
+    }
+    
+    public List<AuthUsers> getFilteredAuthUsers(String searchKey){
+    	return authusersRepository.getFilteredAuthUsers(searchKey);
+    }
+}
