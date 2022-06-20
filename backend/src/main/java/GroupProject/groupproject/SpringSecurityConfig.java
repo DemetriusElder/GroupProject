5eb3ff069b3a607927bbc13767f8b7e9264fa4d9
DemetriusElder <104701476+DemetriusElder@users.noreply.github.com>
2022-06-20 18:06:32 -0500

    Add files via upload

--- backend/src/main/java/GroupProject/groupproject/SpringSecurityConfig.java
+++ backend/src/main/java/GroupProject/groupproject/SpringSecurityConfig.java
@@ -1,25 +1,37 @@
 package GroupProject.groupproject;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.*;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+ 
 @Configuration
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	@Autowired
+	private DataSource dataSource;
+	 
+	@Autowired
+    public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+            .dataSource(dataSource)
+            .usersByUsernameQuery("select username, password, 'true' as enabled from AUTH_USERS where username=?")
+            .authoritiesByUsernameQuery("select username, role from AUTH_USERS where username=?")
+        ;
+    }
+	
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().
-        disable()
-            .authorizeRequests()
-            .antMatchers(HttpMethod.OPTIONS, "/**")
-            .permitAll()
-            .anyRequest()
-            .authenticated()
-            .and()
-            .httpBasic();
+    	 http.authorizeRequests()
+         .anyRequest().authenticated()
+         .and()
+         .formLogin().permitAll()
+         .and()
+         .logout().permitAll(); 
     }
 }
