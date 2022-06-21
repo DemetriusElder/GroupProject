73f98cca9ee3ea5e804d05dcb7f5457742759f23
Tam Doan <mdtam147@gmail.com>
2022-06-20 19:27:19 -0500

    database for users and bcrypt auth

--- backend/src/main/java/GroupProject/groupproject/SpringSecurityConfig.java
+++ backend/src/main/java/GroupProject/groupproject/SpringSecurityConfig.java
@@ -4,6 +4,7 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.*;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
@@ -27,11 +28,14 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-    	 http.authorizeRequests()
-         .anyRequest().authenticated()
+    	 http.csrf().
+         disable()
+         .authorizeRequests()
+         .antMatchers(HttpMethod.OPTIONS, "/**")
+         .permitAll()
+         .anyRequest()
+         .authenticated()
          .and()
-         .formLogin().permitAll()
-         .and()
-         .logout().permitAll(); 
+         .httpBasic();
     }
 }
