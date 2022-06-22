e7a46fd6adb23d87452d1fa083bf0eeedbb78cda
DemetriusElder <104701476+DemetriusElder@users.noreply.github.com>
2022-06-22 01:36:22 -0500

    Add files via upload

--- backend/src/main/java/GroupProject/groupproject/dto/AuthUsersDto.java
+++ backend/src/main/java/GroupProject/groupproject/dto/AuthUsersDto.java
@@ -1,14 +1,17 @@
 package GroupProject.groupproject.dto;
 
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
 public class AuthUsersDto {
 	
 	private final String username;
     private final String password;
     private final String role;
+    String salt = BCrypt.gensalt(12);
     
     public AuthUsersDto(String username, String password, String role) {
         this.username = username;
-        this.password = password;
+        this.password = BCrypt.hashpw(password, salt);
         this.role = role;
     }
 
