74e9ff54b40d2b6d6fa2875b976fbbfe0175354f
DemetriusElder <104701476+DemetriusElder@users.noreply.github.com>
2022-06-22 19:21:37 -0500

    Add files via upload

--- frontend/src/app/blog-list/blog-list.component.ts
+++ frontend/src/app/blog-list/blog-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Blog } from '../blog/blog';
 import { BlogService } from '../blog/blog.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-blog-list',
@@ -9,8 +10,13 @@ import { BlogService } from '../blog/blog.service';
 })
 export class BlogListComponent implements OnInit {
   blogs: Blog[] = [];
+  count: any = 0;
+  max: number = 0;
+  pagenumber: number = 1;
+  private blogsUrl = 'http://localhost:8080/entries';
 
-  constructor(private blogService: BlogService) {}
+  constructor(private blogService: BlogService,
+    private http: HttpClient) {}
 
   ngOnInit(): void {
     this.getBlogs();
@@ -18,7 +24,17 @@ export class BlogListComponent implements OnInit {
 
   getBlogs() {
     this.blogService
-      .getBlogs()
-      .subscribe((blogs) => (this.blogs = blogs.reverse()));
+      .getPageBlogs(this.pagenumber)
+      .subscribe((blogs) => (this.blogs = blogs));
+      this.blogService.getCount().subscribe((y: number)=> 
+      {
+        this.count = y;
+        this.max = Math.ceil(this.count /2);
+      });
+  }
+  changePage(x: number){
+    this.max = Math.ceil(this.count /2);
+    this.pagenumber = x;
+    console.log(this.max);
   }
 }
