5f8e1d1085555be7b93e33538e45653c136d51f8
Hoang Le <hoangp.le21@gmail.com>
2022-06-25 17:09:16 -0500

    Added post form page

--- frontend/src/app/components/add-page/add-page.component.ts
+++ frontend/src/app/components/add-page/add-page.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-page',
   templateUrl: './add-page.component.html',
-  styleUrls: ['./add-page.component.css']
+  styleUrls: ['./add-page.component.css'],
 })
-export class AddPageComponent implements OnInit {
+export class AddPageComponent {
+  postForm = new FormGroup(
+    {
+      title: new FormControl('', [
+        Validators.required,
+        Validators.minLength(20),
+      ]),
+      imageUrl: new FormControl('', [Validators.required]),
+      content: new FormControl('', [
+        Validators.required,
+        Validators.minLength(200),
+      ]),
+    },
+    { updateOn: 'submit' }
+  );
 
-  constructor() { }
-
-  ngOnInit(): void {
+  onSubmit(): void {
+    if (!this.postForm.valid) {
+      this.postForm.markAllAsTouched();
+    }
+    console.log(this.postForm.value);
   }
 
+  get title() {
+    return this.postForm.get('title');
+  }
+  get imageUrl() {
+    return this.postForm.get('imageUrl');
+  }
+  get content() {
+    return this.postForm.get('content');
+  }
 }
