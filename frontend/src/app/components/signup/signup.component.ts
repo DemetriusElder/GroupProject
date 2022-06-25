92ea7e62217b445f45c2c6c39a77f20684256b82
Hoang Le <hoangp.le21@gmail.com>
2022-06-24 23:26:57 -0500

    Added error messages

--- frontend/src/app/components/signup/signup.component.ts
+++ frontend/src/app/components/signup/signup.component.ts
@@ -1,20 +1,52 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css'],
 })
-export class SignupComponent {
-  signupForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    username: new FormControl(''),
-    password: new FormControl(''),
-  });
+export class SignupComponent implements OnInit {
+  signupForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup(
+      {
+        firstName: new FormControl('', [Validators.required]),
+        lastName: new FormControl('', [Validators.required]),
+        username: new FormControl('', [
+          Validators.required,
+          Validators.minLength(6),
+        ]),
+        password: new FormControl('', [
+          Validators.required,
+          Validators.minLength(6),
+        ]),
+      },
+      { updateOn: 'submit' }
+    );
+  }
 
   onSubmit(): void {
+    if (!this.signupForm.valid) {
+      this.signupForm.markAllAsTouched();
+    }
     console.log(this.signupForm.value);
+  }
+
+  get firstName() {
+    return this.signupForm.get('firstName');
+  }
+
+  get lastName() {
+    return this.signupForm.get('lastName');
+  }
+
+  get username() {
+    return this.signupForm.get('username');
+  }
+
+  get password() {
+    return this.signupForm.get('password');
   }
 }
