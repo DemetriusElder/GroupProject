6f321a64586a54007fffd3dc172deae9bafc6b85
Hoang Le <hoangp.le21@gmail.com>
2022-06-26 08:27:06 -0500

    Fixed links in navbar

--- frontend/src/app/components/navbar/navbar.component.ts
+++ frontend/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from 'src/app/models/user';
 import { AuthService } from 'src/app/services/auth.service';
